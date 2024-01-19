fa4e84946e7ee14cb087c0168c8ffc2f77b8e23c
mdarbaz <zainshaikh2468@gmail.com>
2024-01-19 11:42:44 +0530

    avatar and favicon updated

--- components/about.tsx
+++ components/about.tsx
@@ -35,15 +35,6 @@ export default function About() {
         <span className="font-medium">full-time position</span> as a software
         developer.
       </p>
-
-      {/* <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
-      </p> */}
     </motion.section>
   );
 }
